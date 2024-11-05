9c8109edbe6c5e81ea32c463f447716753d879d8
houser <houserqu@qq.com>
2024-11-05 23:01:33 +0800

    fix: error content-type

--- ahttp/error_hander.go
+++ ahttp/error_hander.go
@@ -9,6 +9,8 @@ import (
 
 // CustomErrorHandler 自定义错误处理器
 func CustomErrorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+	w.Header().Set("Content-Type", "application/json")
+
 	// 始终返回 200
 	w.WriteHeader(http.StatusOK)
 	runtime.DefaultHTTPErrorHandler(ctx, mux, marshaler, w, r, err)
