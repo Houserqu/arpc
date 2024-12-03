ef8be52b307911f2b54a1f5bb67c2bf76ff51aed
houser <houserqu@qq.com>
2024-12-03 15:31:03 +0800

    feat: 支持通过 mate data 自定义 grpc gateway http status code

--- ahttp/error_hander.go
+++ ahttp/error_hander.go
@@ -3,6 +3,7 @@ package ahttp
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
@@ -11,7 +12,19 @@ import (
 func CustomErrorHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
 	w.Header().Set("Content-Type", "application/json")
 
-	// 始终返回 200
-	w.WriteHeader(http.StatusOK)
+	// 默认返回 200
+	httpStatusCode := 200
+
+	md, ok := runtime.ServerMetadataFromContext(ctx)
+	if ok {
+		// 读取 gRPC Header 和 Trailer 中的 grpc-gateway-http-status
+		if values, exist := md.HeaderMD["grpc-gateway-http-status"]; exist {
+			httpStatusCode, _ = strconv.Atoi(values[0])
+		} else if values, exist := md.TrailerMD["grpc-gateway-http-status"]; exist {
+			httpStatusCode, _ = strconv.Atoi(values[0])
+		}
+
+	w.WriteHeader(httpStatusCode)
+
 	runtime.DefaultHTTPErrorHandler(ctx, mux, marshaler, w, r, err)
 }
